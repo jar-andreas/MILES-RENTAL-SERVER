9ca996c2317e182217eccd1f093d5016a9afe658
jar-andreas <adeyinkaayomide45@gmail.com>
2026-05-06 04:00:41 -0500

    feat: included more models as seen in the car details page

--- src/models/car.model.ts
+++ src/models/car.model.ts
@@ -1,8 +1,8 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 export interface UserCar extends Document {
-  _id: mongoose.Types.ObjectId;
   brand: string;
+  description: string;
   segment:
     | "EXECUTIVE"
     | "LOGISTICS"
@@ -10,15 +10,26 @@ export interface UserCar extends Document {
     | "CITY"
     | "PREMIUM"
     | "ELECTRIC";
+  tags: ("CITY" | "BEST SELLER" | "ECONOMY" | "POPULAR")[]; // ["BEST SELLER", "ECONOMY"]
   category: string;
   modelName: string;
+  year: number;
   pricePerDay: number;
-  capacity: number;
+  seats: number;
   fuelType: string;
+  features: string[];
   transmission: "Auto" | "Manual" | string;
   image: {
     url: string;
     public_id: string;
+  }[];
+  rating: number;
+  tripsCount: number;
+  carSpecs: {
+    engine: string; // 3.5L V6
+    topSpeed: string; // 230 km/h
+    mileage: string; // 22 km/L
+    boot: string; // 454 L
   };
   slug: string;
 }
@@ -30,14 +41,18 @@ const CarSchema = new Schema<UserCar>(
       required: true,
       trim: true,
     },
-
+    description: {
+      type: String,
+      required: [true, "Please provide a description for the car"],
+      trim: true,
+      maxlength: [1000, "Description cannot exceed 1000 characters"],
+    },
     segment: {
       type: String,
       required: true,
-      uppercase: true, // Automatically saves as uppercase to match Figma
+      uppercase: true,
       enum: ["EXECUTIVE", "LOGISTICS", "FAMILY", "CITY", "PREMIUM", "ELECTRIC"],
     },
-
     category: {
       type: String,
       required: true,
@@ -49,34 +64,64 @@ const CarSchema = new Schema<UserCar>(
       required: true,
       trim: true,
     },
+    year: { type: Number, required: true },
+    tags: {
+      type: [String],
+      required: true,
+      uppercase: true,
+      enum: ["CITY", "BEST SELLER", "ECONOMY", "POPULAR"],
+      default: ["POPULAR"],
+    },
     pricePerDay: {
       type: Number,
       required: true,
       min: [0, "Price per day cannot be negative"],
     },
-    capacity: {
+    seats: {
       type: Number,
       required: true,
-      min: [1, "Capacity must be at least 1"],
+      min: [1, "Seat must be at least 1"],
     },
     fuelType: {
       type: String,
       required: true,
       trim: true,
     },
+    rating: { type: Number, default: 5 },
+    tripsCount: { type: Number, default: 0 },
     transmission: {
       type: String,
       required: true,
-      enum: ["Auto", "Manual"],
+      enum: ["AUTO", "MANUAL"],
+      uppercase: true,
     },
-    image: {
-      url: { type: String, required: true },
-      public_id: { type: String, required: true },
+    features: {
+      type: [String],
+      default: [
+        "Comprehensive insurance",
+        "24/7 road support",
+        "Free Cancellation",
+        "Unlimited mileage in-city",
+        "Sanitize between trips",
+        "Full tank at pickup",
+      ],
+    },
+    image: [
+      {
+        url: { type: String, required: true },
+        public_id: { type: String, required: true },
+      },
+    ],
+    carSpecs: {
+      engine: String,
+      topSpeed: String,
+      mileage: String,
+      boot: String,
     },
     slug: {
-            type: String,
-            unique: true,
-        },
+      type: String,
+      unique: true,
+    },
   },
   {
     timestamps: true,
