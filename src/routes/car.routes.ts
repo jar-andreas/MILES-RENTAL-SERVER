e9d0b2b48eccd65dd243d70209c2cf6d46137abb
jar-andreas <adeyinkaayomide45@gmail.com>
2026-05-07 05:03:16 -0500

    feat: included controller to fetch trending cars

--- src/routes/car.routes.ts
+++ src/routes/car.routes.ts
@@ -3,12 +3,14 @@ import {
   createCar,
   getAllCars,
   getSingleCar,
+  getTrendingCars,
 } from "src/controllers/car.controller.js";
 
 const router = Router();
 
 router.post("/create", createCar);
 router.get("/all", getAllCars);
+router.get("/trending", getTrendingCars);
 router.get("/single/:slug", getSingleCar);
 
 export default router;
