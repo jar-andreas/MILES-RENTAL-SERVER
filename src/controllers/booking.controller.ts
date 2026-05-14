70b8220f925952d29537f7f113e3eb250a59eedc
charles-uix <amadiidinma39@gmail.com>
2026-05-14 13:33:38 +0100

    updated code

--- src/controllers/booking.controller.ts
+++ src/controllers/booking.controller.ts
@@ -92,8 +92,6 @@ export const createBooking = tryCatchWrapper(
       totalDays,
       totalPrice,
       driverOption,
-      driverFee,
-      serviceFee,
     });
 
     return sendTsRestSuccess(res, 201, {
