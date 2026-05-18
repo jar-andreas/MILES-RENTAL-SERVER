41d40385b9a1ba753f0b49ecac5f1c1934374e64
jar-andreas <adeyinkaayomide45@gmail.com>
2026-05-18 06:58:54 -0500

    feat: cleaned up the callback url for payment

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -3,8 +3,30 @@ import Booking from "src/models/booking.model.js";
 import tryCatchWrapper from "src/lib/tryCatchWrapper.js";
 import { sendTsRestError, sendTsRestSuccess } from "src/lib/responseHandler.js";
 
-export const getAllBookings = tryCatchWrapper(
+export const getAdminBookings = tryCatchWrapper(
   async (req: Request, res: Response) => {
-    const { page = 1, limit = 10 } = req.query;
+    const {
+      page = 1,
+      limit = 10,
+      bookingStatus,
+      pickupLocation,
+      pickupDate,
+      query,
+      returnDate,
+      pickupTime,
+      returnTime,
+    } = (req as any).query;
+    const sanitizeQuery = query
+      ? query.toLowerCase().replace(/[^\w\s]/gi, "")
+      : "";
+    const sanitizePickupDate = pickupDate ? new Date(pickupDate) : null;
+    const sanitizeReturnDate = returnDate ? new Date(returnDate) : null;
+    const getUsers = await User.find({
+      $or: [
+        { firstName: { $regex: sanitizeQuery, $options: "i" } },
+        { lastName: { $regex: sanitizeQuery, $options: "i" } },
+      ],
+    }).lean();
+    const matchUserIds = getUsers.map((user) => user._id);
   },
 );
