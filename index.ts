712a8423c4c95e1fb45cc9bc8a514a8d74e25d57
jar-andreas <adeyinkaayomide45@gmail.com>
2026-06-08 06:48:46 -0500

    feat: included the local host for the allow access origin

--- index.ts
+++ index.ts
@@ -96,6 +96,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   // Allow credentials
   res.header(
     "Access-Control-Allow-Origin",
+    "http://localhost:4500",
     "https://milescar-rental.vercel.app",
   );
   res.header("Access-Control-Allow-Credentials", "true");
