1f59d08604b67edf9adbfe017c2c22bb935bbb93
sayo <sayowayne7@gmail.com>
2026-06-09 05:19:18 -0700

    minimal changes

--- index.ts
+++ index.ts
@@ -94,6 +94,7 @@ app.use(createExpressLogger());
 app.use(cors(corsOptions));
 app.use((req: Request, res: Response, next: NextFunction) => {
   // Allow credentials
+  res.header("Access-Control-Allow-Origin", "https://milescar-rental.vercel.app");
   res.header("Access-Control-Allow-Credentials", "true");
   // Handle preflight
   if (req.method === "OPTIONS") {
